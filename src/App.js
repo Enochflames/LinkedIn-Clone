ca401f8cd1f436c4a44f9721b5d03d93a32e9c1d
Enochflames <114030521+Enochflames@users.noreply.github.com>
2024-10-01 21:30:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Network from "./pages/Network";
-import Jobs from "./pages/Jobs";
-import Notifications from "./pages/Notifications";
+import Login from "./components/Login"; 
+import Home from "./pages/Home"; 
+
 
 function App() {
   const [username, setUsername] = useState("");
@@ -11,11 +10,27 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
+      
+        <Route path="/login" element={<Login setUsername={setUsername} />} />
+
+        
         <Route path="/home" element={<Home username={username} showRightside={true} />} />
-        <Route path="/network" element={<Home username={username} showRightside={false} content="network" />} />
-        <Route path="/jobs" element={<Home username={username} showRightside={false} content="jobs" />} />
-        <Route path="/notifications" element={<Home username={username} showRightside={false} content="notifications" />} />
+
+        <Route
+          path="/Network"
+          element={<Home username={username} showRightside={false} content="network" />}
+        />
+        <Route
+          path="/Jobs"
+          element={<Home username={username} showRightside={false} content="jobs" />}
+        />
+        <Route
+          path="/Notifications"
+          element={<Home username={username} showRightside={false} content="notifications" />}
+        />
+
+        
+        <Route path="*" element={<Navigate to={username ? "/home" : "/login"} />} />
       </Routes>
     </Router>
   );
